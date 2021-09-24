f7dffa4ee90d0ff0ef1c96ffb32f69e178df30aa
FedeScholten <fede.scholten@gmail.com>
2021-09-24 13:04:06 -0300

    java

---
+++ Javascript/JS/Java.js
@@ -0,0 +1,36 @@
+alert('que onda turro de la calle');
+       console.log('puto de mierda aguerrido');
+       var nombre_variable;
+       let otra_variable = "Hola";
+       const pi = 3.1416;
+       let num2 = 18;
+       let hayClases = true;
+       let terminoLaClase = false;
+       console.log(pi*num2);
+       console.log(num2 + 36 + " " + otra_variable)
+       if(otra_variable == "Hola" && hayClases != false){
+           console.log('vamos a la videollamada');
+       } else{
+           console.log('nos la re tomamos ñeri');
+       }
+       if(hayClases == false || terminoLaClase == false){
+           console.log('reeee picuso');
+       } else{
+           console.log('malardovich');
+       }
+
+       let elemento = document.getElementById("principal");
+       //    console.log(elemento);
+       elemento.innerHTML ="buenas tardes"
+       elemento.innerHTML += "<p text-right bg-warning>" + otra_variable + "</p>"
+       document.write("<h3 class= 'text-center bg-primary'>Santi chiquito</h3>");
+
+       if(nombre_variable == undefined){
+           elemento.innerHTML += "estamos de parranda"
+           elemento.innerHTML += "<p>Ema</p>"
+           elemento.innerHTML += "<p>Natha</p>"
+           elemento.innerHTML += "<p>Mata</p>"
+           elemento.innerHTML += "<p>Santi</p>"
+           elemento.style.background = "Black"
+           elemento.style.color = "White"
+       }
