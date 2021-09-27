e8c2e04061d0a7dd04a70e82502cdaa87f879396
FedeScholten <fede.scholten@gmail.com>
2021-09-27 16:39:09 -0300

    Java

--- Javascript/JS/Java.js
+++ Javascript/JS/Java.js
@@ -1,8 +1,9 @@
-alert('que onda turro de la calle');
+// alert('que onda turro de la calle');
        console.log('puto de mierda aguerrido');
        var nombre_variable;
        let otra_variable = "Hola";
        const pi = 3.1416;
+       let num = 15;
        let num2 = 18;
        let hayClases = true;
        let terminoLaClase = false;
@@ -31,6 +32,68 @@ alert('que onda turro de la calle');
            elemento.innerHTML += "<p>Natha</p>"
            elemento.innerHTML += "<p>Mata</p>"
            elemento.innerHTML += "<p>Santi</p>"
-           elemento.style.background = "Black"
-           elemento.style.color = "White"
+        //    elemento.style.background = "Black"
+        //    elemento.style.color = "white"
        }
+
+    //    let mensaje = prompt('introdice tu mensaje');  
+    //    elemento.innerHTML += "<h3>" + mensaje + "</h3>";
+    //    elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">"+ mensaje +"</div>"
+
+    // let menu = prompt('Comete alguna de estas, y esta \n 1. Tu vieja \n 2. Tu hermana \n 3. Tu suerga');
+
+
+    // if(menu == 1){
+    //     elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">Sos un asco hermano</div>"
+    // } else if(menu == 2){
+    //     elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">Bueno no está mal</div>"
+    // } else if(menu == 3){
+    //     elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">Sos un asco man</div>"
+    // } else {
+    //     alert('elegi uno down');
+    //     let menu = prompt('Comete alguna de estas, y esta \n 1. Tu vieja \n 2. Tu hermana \n 3. Tu suerga');
+    // }
+
+    // switch (menu){
+    //     case 1 :
+    //         elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">Sos un asco hermano</div>"
+    //         break;
+    //     default:
+    //         elemento.innerHTML += "<div class=\"alert alert-success\" role=\"alert\">'elegi uno down'</div>"
+    //         break;
+    // }
+
+    // let num3 = Number(prompt(' ingresa el primer numero'));
+    // let num4 = Number(prompt(' ingresa el segundo numero'));
+
+    // let resultado = "El resultado de la suma de "+ num3 +" y "+ num4 +" es: " + (num3 + num4);
+
+    let arreglo = ["emin", "natha", "santi", "mata", "fede" ,"asa", "yoe"];
+
+    for(let b = 0; b < arreglo.length; b++){
+        elemento.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${arreglo[b]}</h5>
+            <p class="card-text"</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
+    `;
+    }
+
+arreglo.push("dupla");
+arreglo[8]= "Juan"
+
+    arreglo.forEach((b,index)=>{
+        elemento.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${index + 1}${b}</h5>
+            <p class="card-text"</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>`
+    });
